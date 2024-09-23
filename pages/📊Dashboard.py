e2d1d23cbb4013af9413527e769134b0fe1013a9
Debadri Dutta <debadridtt@gmail.com>
2024-09-24 01:27:34 +0530

    #2

--- pages/📊Dashboard.py
+++ pages/📊Dashboard.py
@@ -4,3 +4,8 @@ import altair as alt
 from urllib.error import URLError
 
 st.set_page_config(page_title="Griharaj: Insights", page_icon="📊")
+
+st.markdown("# Properties Insights")
+st.sidebar.header("Properties Insights")
+
+df = pd.read_csv('Datasets/properties.csv')
